268945e19c0ff3699e73444ddf71019186b89829
sagaasachin <sagaasachin48@gmail.com>
2025-08-08 17:05:25 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/AutoSpares/', // Must match your GitHub repo name!
+  base: '/Spares/', // Must match your GitHub repo name!
   plugins: [react()]
 })
